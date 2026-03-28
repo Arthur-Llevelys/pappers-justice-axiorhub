65431f211a3c4bb22a6351c37a644fe719ec5c0b
Arthur-Llevelys <101706410+Arthur-Llevelys@users.noreply.github.com>
2026-03-28 11:57:23 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
+
 from pappers_mcp.server import create_mcp
 
 mcp = create_mcp()
 
 if __name__ == "__main__":
-    mcp.run()
+    mcp.run(
+        transport="http",
+        host="0.0.0.0",
+        port=8001,
+        path="/mcp/",
+    )
+
+
